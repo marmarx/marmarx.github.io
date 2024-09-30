3a90ef3f836d89f3e6b0a75f34f02f423df544ea
marmarx <78326372+marmarx@users.noreply.github.com>
2024-09-30 12:45:17 -0300

    Delete t20/js/power_worker.js

--- t20/js/power_worker.js
+++
@@ -1,7 +0,0 @@
-function worker_list(j,arr,min,max){
- let func = ['equip','armor','weap','feat'];
- let div = '';
- for(var i=min;i<max;i++){div += '<a onclick="'+func[j]+'_choose(this,'+i+')" tabindex="0">'+arr[i][0]+'</a>'}
- return div;
-}
-onmessage = function(ev){postMessage(worker_list(ev.data[0],ev.data[1],ev.data[2],ev.data[3]))}
