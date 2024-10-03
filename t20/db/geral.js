e02ef428b44d4b6c105c737ccf714e4588859d55
marmarx <78326372+marmarx@users.noreply.github.com>
2024-10-03 14:43:09 -0300

    Add files via upload

--- t20/db/geral.js
+++ t20/db/geral.js
@@ -2,7 +2,6 @@ const habs = ['spell_hab','for','des','con','int','sab','car'];
 const habis = ['Habilidade','Força','Destreza','Constituição','Inteligência','Sabedoria','Carisma'];
 const nivel = ['nível',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 const tamanho = ['tamanho','Minúsculo','Pequeno','Médio','Grande','Enorme','Colossal'];
-const rd = ['Ácido', 'Eletricidade', 'Fogo', 'Frio', 'Luz', 'Trevas', 'Mágico'];
 const action = ['Ação para ativar','Nenhuma Ação','Ação Livre','Ação de Movimento','Ação Padrão','Ação Completa','Reação'];
 const duration = ['Duração','Instantânea','Até seu próximo turno','Sustentada','Cena','Dia','Semana','O tempo que quiser','Permanente'];
 const power_typ = ['Tipo de poder','Raça','Origem','Classe','Distinção de Classe','Geral'];
@@ -14,11 +13,5 @@ const deslocamento = [
 'natação (6m)','natação (9m)','natação (12m)','natação (15m)','natação (18m)','natação (24m)','natação (30m)',
 'vôo (6m)','vôo (9m)','vôo (12m)','vôo (15m)','vôo (18m)','vôo (24m)','vôo (30m)'
 ];
-
-const raca = ['raça','Anão','Dahllan','Elfo','Goblin','Golem','Humano','Hynne','Kliren','Lefou','Medusa','Minotauro','Osteon','Qareen','Sereia/Tritão','Sílfide','Suraggel: Aggelus','Suraggel: Sulfure','Trog','Meio-Orc','Orc','Tabrachi','Ogro','Bugbear','Hobgoblin','Centauro','Gnoll','Kallyanach','Kaijin','Kappa','Mashin','Nezumi','Tengu','Minauro','Kobolts','Harpia','Ceratops','Pteros','Velocis','Voracis','Yidishan','Moreau','Elfo-do-Mar','Nagah','Finntroll'];
-const classe = ['classe','Arcanista','Bárbaro','Bardo','Bucaneiro','Caçador','Cavaleiro','Clérigo','Druida','Guerreiro','Inventor','Ladino','Lutador','Miragem','Místico','Nobre','Paladino','Samurai'];
-const divindade = ['divindade','Aharadak','Allihanna','Arsenal','Azgher','Hyninn','Kallyadranoch','Khalmyr','Lena','Lin-Wu','Marah','Megalokk','Nimb','Oceano','Sszzaas','Tanna-Toh','Tenebra','Thwor','Thyatis','Valkaria','Wynna','O Bem','O Mal','O Panteão'];
-
-const origem = ['origem','Acólito','Amigo dos Animais','Amnésico','Aristocrata','Artesão','Artista','Assistente de Laboratório','Batedor','Capanga','Charlatão','Circense','Criminoso','Curandeiro','Eremita','Escravo','Estudioso','Fazendeiro','Forasteiro','Gladiador','Guarda','Herdeiro','Herói Camponês','Marujo','Mateiro','Membro de Guilda','Mercador','Minerador','Nômade','Pivete','Refugiado','Seguidor','Selvagem','Soldado','Taverneiro','Trabalhador','Agricultor Sambur','Amazona de Hippion','Amoque Púrpura','Anão de Armas','Andarilho Ubaneri','Aprendiz de Dragoeiro','Aprendiz de Drogadora','Aristocrata Dai’zenshi','Armeiro Armado','Aspirante a Herói','Assistente Forense','Bandoleiro da Fortaleza','Barão Arruinado','Catador da Cidade Velha','Cativo das Fadas','Competidor do Circuito','Cosmopolita','Cria da Favela','Criado pelas Voracis','De Outro Mundo','Descendente Colleniano','Desertor da Supremacia','Duplo Feérico','Duyshidakk Infiltrado','Emissário Ubaner','Escudeiro da Luz','Escudeiro Solitário','Estandarte Vivo','Estudante da Academia','Estudante do Colégio Real','Explorador de Ruínas','Filhote da Revoada','Futura Lenda','Ginete de Tumarkhân','Grumete Pirata','Guardião Glacial','Iniciado dos Caça-Monstros','Insurgente Tapistano','Irmão sem Esporas','Legionário','Lenhador de Tollon','Liricista de Lenórienn','Membro do Principado','Nitamuraniano','Nobre Zakharoviano','Nômade Sar-Allan','Pescador Parrudo','Plebeu Arcano','Prisioneiro das Catacumbas','Procurado: Vivo ou Morto','Profeta do Akzath','Querido Filho','Rebelde Agitador','Receptador das Nuvens','Recruta Arcano','Recruta da Fênix','Sábio Matemático','Selvagem Sanguinário','Sucateiro de Batalhas','Tamalu','Tocado pela Dama Altiva','Tocado pelo Indomável','Tradicionalista Svalano','Trapaceiro Ahleniense','Turista da Academia','Um com os Kami'];
 
 const inputs = [raca,origem,nivel,tamanho,habs];
