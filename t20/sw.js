7c8ed296a31c44a521cd690aa8ba6623ad5e8252
marmarx <78326372+marmarx@users.noreply.github.com>
2024-10-29 15:54:44 -0300

    Update sw.js

--- t20/sw.js
+++ t20/sw.js
@@ -7,7 +7,7 @@ const cache_resources = ['/t20/','/t20/print.html',
 
 //Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
- console.log('Service worker install event!');
+ alert('Service worker install event!');
  event.waitUntil((async () => {
   const cache = await caches.open(cache_name);
   cache.addAll(cache_resources);
@@ -15,20 +15,21 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', (event) => {
- console.log('Service worker activate event!');
+ alert('Service worker activate event!');
 });
 
 self.addEventListener('fetch', event => {
- console.log('Fetch intercepted for:', event.request.url);
+ alert('Fetch intercepted for:', event.request.url);
  event.respondWith((async () => {
  const cache = await caches.open(cache_name);
 
  const cachedResponse = await cache.match(event.request);
- if(cachedResponse){return cachedResponse}	// Get the resource from the cache.
+ if(cachedResponse){alert('Fetch file offline');return cachedResponse}	// Get the resource from the cache.
  else{						//If the resource was not in the cache, try the network.
   try{
    const fetchResponse = await fetch(event.request);
    cache.put(event.request, fetchResponse.clone());	//Save the resource in the cache and return it.
+   alert('Trying to fetch file online');
    return fetchResponse;
    }catch(e){alert('Error: '+e)}
   }
